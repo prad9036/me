ed0f1c6378541a43363458bf668e8c6525ca99c4
prad9036 <187216155+prad9036@users.noreply.github.com>
2026-02-25 22:18:48 +0530

    Update plgtest.sh

--- plgtest.sh
+++ plgtest.sh
@@ -24,7 +24,7 @@ fi
 export PATH="$HOME/.local/bin:$PATH"
 
 echo "[+] Downloading and extracting project..."
-curl -o plgb-master.zip http://pprdp.pythonanywhere.com/static/plgb-master.zip
+curl -L -o plgb-master.zip http://pprdp.pythonanywhere.com/static/plgb-master.zip
 python3 -m zipfile -e plgb-master.zip .
 
 cd plgb-master
